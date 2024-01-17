7ba84e027cfcdf628e9b1bbbb4c88b22ff09b18f
Guto <gutomarzagao@gmail.com>
2024-01-17 20:03:10 -0300

    .

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+
+export default function TabOneScreen() {
+  return <Text>Oi</Text>;
+}
+
+const styles = StyleSheet.create({});
