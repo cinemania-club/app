305eefa5178f294dab951dd320d94319a9a49c42
root <root@bruno-windows>
2024-01-17 21:05:17 -0300

    .

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,45 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function TabOneScreen() {
-  return <Text>Oi</Text>;
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headTitle}>
+        Listando <Text style={styles.movieCount}>1234</Text> itens
+      </Text>
+      <View style={styles.card}>
+        <View style={styles.upperSubCard}>Filme 1</View>
+        <View style={styles.bottomSubCard}>Filme 1</View>
+      </View>
+
+      <View>
+        <Text>Filme 2</Text>
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000000",
+    flex: 1,
+  },
+  headTitle: {
+    textAlign: "center",
+    color: "white",
+    margin: 30,
+  },
+  movieCount: {
+    color: "#F9284E",
+  },
+  card: {
+    overflow: "hidden",
+    borderRadius: 80,
+    margin: 16,
+  },
+  upperSubCard: {
+    backgroundColor: "#181818",
+  },
+  bottomSubCard: {
+    backgroundColor: "#202020",
+  },
+});
