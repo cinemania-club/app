d4e6d67c58e7f01a108eac4b075c4ce3ec4c10b3
bruno <brunomarfranco@gmail.com>
2024-01-25 19:21:20 -0300

    .

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { Redirect } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+import Movie from "./movie";
 
 export default function Home() {
   return (
@@ -8,73 +8,8 @@ export default function Home() {
       <Text style={styles.headTitle}>
         Listando <Text style={styles.movieCount}>1234</Text> itens
       </Text>
-      <View style={styles.card}>
-        <View style={styles.upperSubCard}>
-          <Image
-            source={require("../assets/images/posters/oppenheimer-poster.webp")}
-            style={styles.upperSubCardPosterImage}
-          />
-          <View style={[styles.UpperSubCardInfo]}>
-            <Text style={styles.upperSubCardMovieTitle}>Oppenheimer</Text>
-            <View style={[styles.upperSubCardMiddleRow]}>
-              <View style={[styles.upperSubCardEmoji]}>
-                <FontAwesome5
-                  name="laugh-squint"
-                  size={24}
-                  color="white"
-                  style={styles.emoji}
-                />
-                <FontAwesome5
-                  name="grin-hearts"
-                  size={24}
-                  color="white"
-                  style={styles.emoji}
-                />
-                <FontAwesome5 name="surprise" size={24} color="white" />
-              </View>
-              <Text style={styles.text}>2023</Text>
-              <Text style={styles.text}>185 min</Text>
-              <Text style={styles.text}>4,53</Text>
-            </View>
-            <View style={styles.profiles}>
-              <Image
-                source={require("../assets/images/posters/oppenheimer-poster.webp")}
-                style={styles.lowerIconProfile}
-              />
-              <Image
-                source={require("../assets/images/posters/oppenheimer-poster.webp")}
-                style={styles.lowerIconProfile}
-              />
-            </View>
-          </View>
-        </View>
-        <View style={styles.bottomSubCard}>
-          <Image
-            source={require("../assets/images/favicon.png")}
-            style={styles.bottomSubCardIcons}
-          />
-          <Image
-            source={require("../assets/images/favicon.png")}
-            style={styles.bottomSubCardIcons}
-          />
-          <Image
-            source={require("../assets/images/favicon.png")}
-            style={styles.bottomSubCardIcons}
-          />
-          <Image
-            source={require("../assets/images/favicon.png")}
-            style={styles.bottomSubCardIcons}
-          />
-          <Image
-            source={require("../assets/images/favicon.png")}
-            style={styles.bottomSubCardIcons}
-          />
-        </View>
-      </View>
-
-      <View>
-        <Text>Filme 2</Text>
-      </View>
+      <Movie />
+      <Movie />
     </View>
   );
 }
@@ -91,67 +26,5 @@ const styles = StyleSheet.create({
   },
   movieCount: {
     color: "#F9284E",
-  },
-  card: {
-    overflow: "hidden",
-    borderRadius: 16,
-    margin: 16,
-  },
-  upperSubCard: {
-    flexDirection: "row",
-    height: 128,
-  },
-  upperSubCardPosterImage: {
-    width: 100,
-    height: 150,
-    resizeMode: "stretch",
-  },
-  UpperSubCardInfo: {
-    flex: 1,
-    padding: 12,
-    backgroundColor: "#181818",
-  },
-  upperSubCardMovieTitle: {
-    color: "white",
-    marginBottom: 16,
-  },
-  upperSubCardMiddleRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    color: "white",
-  },
-  upperSubCardEmoji: {
-    flexDirection: "row",
-  },
-  emoji: {
-    marginRight: 12,
-  },
-  bottomSubCard: {
-    backgroundColor: "#202020",
-    height: 64,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  bottomSubCardIcons: {
-    width: 32,
-    height: 32,
-    margin: 16,
-  },
-  lowerIconProfile: {
-    height: 30,
-    width: 30,
-    borderColor: "#F9284E",
-    borderRadius: 15,
-    borderWidth: 2,
-    marginTop: 12,
-    marginRight: 5,
-  },
-  profiles: {
-    flexDirection: "row",
-  },
-  text: {
-    color: "white",
   },
 });
