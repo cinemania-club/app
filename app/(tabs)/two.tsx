60b8f31a468fcd88a6419d411fd05795d646a606
bruno <brunomarfranco@gmail.com>
2024-03-06 21:01:12 -0300

    .

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,35 +1,74 @@
-import { StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import {
+  CheckIcon,
+  Checkbox,
+  CheckboxIcon,
+  CheckboxIndicator,
+  CheckboxLabel,
+  HStack,
+  ScrollView,
+  Text,
+  VStack,
+} from "@gluestack-ui/themed";
+import React from "react";
 
-import EditScreenInfo from "../../components/EditScreenInfo";
-import { Text, View } from "../../components/Themed";
+import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 
-export default function TabTwoScreen() {
+export default function Filter() {
+  const primaryColor = gluestackUIConfig.tokens.colors.primary;
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    <ScrollView bg="$darkBackground" flex={1} p="$4">
+      <VStack bg="$mediumBackground" p="$4" space="md">
+        <HStack space="md">
+          <FontAwesome name="filter" size={24} color={primaryColor} />
+          <Text bold color="$primary">
+            Filtrar Lista
+          </Text>
+        </HStack>
+        <Text bold color="$primary" size="sm">
+          Streamings
+        </Text>
+        <HStack>
+          <Text color="$text">Filtrando por seus streamings</Text>
+          <Text color="$primary">(8)</Text>
+        </HStack>
+        <HStack>
+          <Text color="$primary">Editar streamings</Text>
+        </HStack>
+        <Text color="$primary">Tipo</Text>
+        <HStack>
+          <Checkbox size="md">
+            <CheckboxIndicator mr="$2">
+              <CheckboxIcon as={CheckIcon} />
+            </CheckboxIndicator>
+            <CheckboxLabel>Label</CheckboxLabel>
+          </Checkbox>
+          <Text color="$text">Filme</Text>
+          <Text color="$text">Série</Text>
+          <Text color="$text">Minissérie</Text>
+        </HStack>
+        <Text color="$primary">Gênero</Text>
+        <HStack flexWrap="wrap">
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+          <Text>Ação</Text>
+        </HStack>
+      </VStack>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
