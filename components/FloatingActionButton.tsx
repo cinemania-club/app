b91256106de979d06890043a3fc350b504117b36
bruno <brunomarfranco@gmail.com>
2024-05-02 20:58:48 -0300

    .

--- components/FloatingActionButton.tsx
+++ components/FloatingActionButton.tsx
@@ -1,11 +1,75 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { Pressable, View } from "react-native";
 
 import s from "../src/theme/styles";
 
 export default function (props: { icons: ReactNode[] }) {
+  const [expanded, setExpanded] = useState(true);
+
   const topIcons = props.icons.filter((_, index) => index % 2 === 1);
   const bottomIcons = props.icons.filter((_, index) => index % 2 === 0);
+
+  if (expanded) {
+    return (
+      <View style={[s.g3, s.m4]}>
+        <Pressable
+          style={[
+            { width: 48, height: 48 },
+            s.rounded,
+            s.bgAccent,
+            s.aiCenter,
+            s.jcCenter,
+            s.g2,
+          ]}
+          onPress={() => {}}
+        >
+          <View style={[s.row, s.g2]}>{topIcons}</View>
+        </Pressable>
+
+        <Pressable
+          style={[
+            { width: 48, height: 48 },
+            s.rounded,
+            s.bgAccent,
+            s.aiCenter,
+            s.jcCenter,
+            s.g2,
+          ]}
+          onPress={() => {}}
+        >
+          <View style={[s.row, s.g2]}>{topIcons}</View>
+        </Pressable>
+
+        <Pressable
+          style={[
+            { width: 48, height: 48 },
+            s.rounded,
+            s.bgAccent,
+            s.aiCenter,
+            s.jcCenter,
+            s.g2,
+          ]}
+          onPress={() => {}}
+        >
+          <View style={[s.row, s.g2]}>{topIcons}</View>
+        </Pressable>
+
+        <Pressable
+          style={[
+            { width: 48, height: 48 },
+            s.rounded,
+            s.bgAccent,
+            s.aiCenter,
+            s.jcCenter,
+            s.g2,
+          ]}
+          onPress={() => {}}
+        >
+          <View style={[s.row, s.g2]}>{topIcons}</View>
+        </Pressable>
+      </View>
+    );
+  }
 
   return (
     <Pressable
@@ -18,6 +82,7 @@ export default function (props: { icons: ReactNode[] }) {
         s.jcCenter,
         s.g2,
       ]}
+      onPress={() => setExpanded(true)}
     >
       <View style={[s.row, s.g2]}>{topIcons}</View>
       <View style={[s.row, s.g2]}>{bottomIcons}</View>
