d51eda8e1d6a0b590c242b4e53bb492a3f9b11af
Wallace Leandro <wallaceleandro2003@gmail.com>
2024-05-18 15:53:00 -0300

    .

--- components/CheckBox.tsx
+++ components/CheckBox.tsx
@@ -1,17 +1,17 @@
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Text, View } from "react-native";
 import { palette } from "../src/theme/colors";
 import s from "../src/theme/styles";
 
-export default function (props: { title: string }) {
+export default function (props: { label: string }) {
   return (
     <View style={[s.row, s.aiCenter, s.g3]}>
-      <MaterialIcons
-        name="check-box-outline-blank"
+      <MaterialCommunityIcons
+        name="checkbox-blank-outline"
         size={24}
         color={palette.primary}
       />
-      <Text style={[s.text]}>{props.title}</Text>
+      <Text style={[s.text]}>{props.label}</Text>
     </View>
   );
 }
--- components/ItemPlaylists.tsx
+++ components/ItemPlaylists.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import s from "../src/theme/styles";
 
 import { palette } from "../src/theme/colors";
@@ -12,29 +12,22 @@ import Overlay from "./Overlay";
 export default function () {
   return (
     <Overlay>
-      <View style={[styles.poster, s.bgLight, s.r4, s.g4, s.p4]}>
-        <Text style={[s.textStrong]}>Adicione à playlist</Text>
-        <CheckBox title="Assistir mais tarde" />
-        <CheckBox title="Arquivados" />
-        <CheckBox title="Filmes para assistir com mozão" />
-        <CheckBox title="Para rir" />
-        <View style={[s.row, s.aiCenter, s.g1]}>
-          <Ionicons name="add-sharp" size={30} color={palette.primary} />
-          <Text style={[s.textPrimary]}>Nova playlist</Text>
+      <View style={[s.aiCenter, s.jcCenter, s.flex1, s.p4]}>
+        <View style={[s.bgLight, s.r4, s.g4, s.p4, { maxWidth: 400 }]}>
+          <Text style={[s.textStrong]}>Adicione à playlist</Text>
+          <CheckBox label="Assistir mais tarde" />
+          <CheckBox label="Arquivados" />
+          <CheckBox label="Filmes para assistir oioioi oioioioioi oioim mozão HHHHHHHHHHHHHHH HHHHHHHHHHHH HHHHHHHH HHHHHHH HHHHHH" />
+          <CheckBox label="Para rir" />
+          <View style={[s.row, s.aiCenter, s.g1]}>
+            <Ionicons name="add-sharp" size={30} color={palette.primary} />
+            <Text style={[s.textPrimary]}>Nova playlist</Text>
+          </View>
+          <Text style={[s.textPrimary, s.taCenter, { fontWeight: "bold" }]}>
+            OK
+          </Text>
         </View>
-        <Text style={[s.textPrimary, s.taCenter, { fontWeight: "bold" }]}>
-          OK
-        </Text>
       </View>
     </Overlay>
   );
 }
-
-const styles = StyleSheet.create({
-  poster: {
-    resizeMode: "cover",
-    marginHorizontal: "auto",
-    marginVertical: "auto",
-    width: "85%",
-  },
-});
