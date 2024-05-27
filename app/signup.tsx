1e33f99100544baa645eeba64102dd7e6b12ae1a
bruno <brunomarfranco@gmail.com>
2024-05-27 20:52:37 -0300

    .

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,6 +1,7 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import React, { ReactNode } from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import ActionButton from "../components/ActionButton";
 import DrawerFrame from "../components/DrawerFrame";
 import TextField from "../components/TextField";
 import { palette } from "../src/theme/colors";
@@ -9,8 +10,10 @@ import s from "../src/theme/styles";
 export default function () {
   return (
     <DrawerFrame title="Cadastro">
-      <Text style={s.text}>LOGO</Text>
-      <View style={s.g4}>
+      <ScrollView
+        style={[s.flex1]}
+        contentContainerStyle={[s.g4, s.p5, s.jcEnd, { minHeight: "100%" }]}
+      >
         <Field title="Email" placeholder="Digite seu email" />
         <Field
           title="Telefone (opcional)"
@@ -38,7 +41,12 @@ export default function () {
             size={20}
           />
         </Field>
-      </View>
+        <ActionButton
+          icon={<MaterialIcons name="login" />}
+          title="Cadastrar"
+          onPress={() => {}}
+        />
+      </ScrollView>
     </DrawerFrame>
   );
 }
@@ -50,7 +58,7 @@ function Field(props: {
   reverse?: boolean;
 }) {
   return (
-    <View style={[s.g2, s.px5]}>
+    <View style={[s.g2]}>
       <Text style={s.textStrong}>{props.title}</Text>
       <TextField placeholder={props.placeholder} reverse={props.reverse}>
         {props.children}
